b67c9973d4a986c966037643cc05612134c097e5
Janis Mozgis <janismozgis@gmail.com>
2019-11-19 19:27:40 +0200

    add fizz bazz app

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ if (confirmOutput == true) {
 } else {
     console.log('boooo!')
 }
+
+
+// fizz bazz
+// Skaita no 0 - 50
+// Ja skaitlis bez atlikuma dalās ar 2, tad izvada Fizz.
+// Ja skaitlis bez atlikuma dalās ar 3, tad izvada Bazz.
+// Ja skaitlsi bez atlikuma dalās ar 2 un 3, tad izvada Fizz Bazz.
+// Ja ne ar vienu, tad izvada slaitli
+
+for (var i = 0; i <= 50; i++) {
+    if (i % 2 == 0 && i % 3 == 0) {
+        console.log('Fizz Bazz')
+    } else if (i % 2 == 0) {
+        console.log('Fizz')
+    } else if (i % 3 == 0) {
+        console.log('Bazz')
+    } else {
+        console.log(i);
+    }
+}
+
+var cars = ['vaz', 'zaz', 'maz', 'gaz'];
+for (var foo of cars) {
+    console.log(foo);
+}
